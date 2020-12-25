9072cb8de42665a8caaeb9e6e28f90a8acf38cef
tenzin <ten13zin@gmail.com>
2020-12-25 15:25:43 +0530

    fix(domain-model): test pass for can_allocate and deallocate

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from typing import Optional
 from datetime import date
+from typing import Optional, Set
+
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -14,7 +15,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
